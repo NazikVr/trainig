d86bae32afc3ad8dc57f40f762fae02ee02565cd
NazikVr <voron1765@gmail.com>
2021-12-07 23:35:38 +0200

    while-for

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+
+// for (let i = 1; i < 6; i++) {
+// 	console.log(i)
+// }
+
+// let i = 5;
+// while (i) {
+// 	console.log(i);
+// 	i--
+// }
+
+// for (let num = 0; num < 3; num++) {
+// 	console.log(`Число: ${num}`);
+// }
+
+// let num = 0;
+// while (num < 3){
+// 	console.log(`Число: ${num}`);
+// 	num++
+// }
+
+// Point:
+// for (let num = 0; num < 2; num++) {
+// 	for (let size = 0; size < 3; size++) {
+// 		console.log(size);
+// 		while (size == 1) break Point
+// 	}
+// }
